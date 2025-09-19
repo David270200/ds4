940c08814360232c6b4464c09e6fc3e1f231ee46
David270200 <david.perez2@utp.ac.pa>
2025-09-19 17:02:17 -0500

    Laboratorio 21 completado

---
+++ Laboratorio2/Laboratorio21/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //asignacion valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        //variable estatica
+        public static int Valor { get; set; }
+    }
+}
