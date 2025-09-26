45fa70d87ff0c8b67bcc144aa6a6eb15554b5165
David270200 <david.perez2@utp.ac.pa>
2025-09-25 20:07:07 -0500

    laboratorio completado

---
+++ Laboratorio5/Laboratorio51/Program.cs
@@ -0,0 +1,30 @@
+﻿private int[] sueldos; //declaramos un vector
+
+public void cargar()
+{
+    sueldos = new int[6] //inicializamos el vector en 5 
+        for (int f = 1; f < 5; f++)
+    {
+        Console.WriteLine("Ingrese sueldo del operario, " + f + ": ");
+        string linea;
+        linea = Console.ReadLine();
+        sueldos[f] = int.Parse(linea); //asignamos los 5 sueldos al vector
+    }
+}
+//muestra los sueldos de los operarios en el vector sueldos[f]
+public void imprimir()
+{
+    Console.WriteLine("Los 5 sueldos de los operarios son: \n");
+    for (int f = 1; f < 5; f++)
+    {
+        Console.WriteLine("Sueldo del operario " + f + ": " + sueldos[f]);
+    }
+    Console.ReadKey();
+}
+//main principal
+static void Main(string[] args)
+{
+    PruebaVector1 pv = new PruebaVector1();
+    lab.cargar(); //llamamos al metodo cargar
+    lab.imprimir(); //llamamos al metodo imprimir
+}
---
+++ Laboratorio5/Laboratorio52/Program.cs
@@ -0,0 +1,24 @@
+﻿private int[,] mat;
+
+public void Ingresar()
+{
+    Mat = new int[3, 4];
+    for (int f = 0; f < 3; f++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            Console.Write("Ingrese posicion [" + (f + 1 + ", " + (c + 1) + "]: ");
+            string linea;
+            linea = Console.ReadLine();
+            mat[f , c] = int.Parse(linea); 
+        }
+    }
+}
+public void Imprimir()
+{
+    for (int f = 0; f < 3; f++)
+    {
+        for (int c = 0; c < 4; c++)
+    }
+    Console.Write(mat[])
+}
