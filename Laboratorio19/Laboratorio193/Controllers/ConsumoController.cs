463384019c4eed375c5784da825ff0fad946c17e
David270200 <david.perez2@utp.ac.pa>
2025-11-25 19:47:52 -0500

    lab 19 completo

---
+++ Laboratorio19/Laboratorio193/Controllers/ConsumoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Laboratorio19_2.Controllers
+{
+    public class ConsumoController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string url = "https://localhost:44363/api/Values/Get/2";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var resultado = await response.Content.ReadAsStringAsync();
+
+                ViewBag.Resultado = resultado;
+            }
+
+            return View();
+        }
+    }
+}
